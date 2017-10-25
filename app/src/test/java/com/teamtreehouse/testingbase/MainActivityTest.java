fcc2dc142acab377ba6e3229ad3199426cd20c1b
Farhan Rahman Arnob <farhanarnob@live.com>
2017-10-26 01:11:05 +0600

    Robolectric Setup

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityTest.java
@@ -1,36 +1,37 @@
 package com.teamtreehouse.testingbase;
 
-import android.view.inputmethod.EditorInfo;
-
 import org.junit.Before;
 import org.junit.Test;
-
-import static junit.framework.Assert.assertEquals;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 /**
  * Created by ${farhanarnob} on ${06-Oct-16}.
  */
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class MainActivityTest {
-    MainActivity activity;
+//    MainActivity activity;
 
     @Before
     public void setUp() throws Exception {
-        activity = new MainActivity();
-        activity.onCreate(null);
+//        activity = new MainActivity();
+//        activity.onCreate(null);
     }
 
     @Test
     public void editTextUpdatesTextView() throws Exception {
-        // Arrange
-        String givenString = "test123";
-        activity.editText.setText(givenString);
-        // Act
-        activity.editText.onEditorAction(EditorInfo.IME_ACTION_DONE);
-
-        // Assert
-
-        String actualString = (String) activity.textView.getText();
-        assertEquals(givenString, actualString);
+//        // Arrange
+//        String givenString = "test123";
+//        activity.editText.setText(givenString);
+//        // Act
+//        activity.editText.onEditorAction(EditorInfo.IME_ACTION_DONE);
+//
+//        // Assert
+//
+//        String actualString = (String) activity.textView.getText().toString();
+//        assertEquals(givenString, actualString);
     }
 
 }
